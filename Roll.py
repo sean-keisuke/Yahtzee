f0c8c3094fb342015fa3962b0056c43d25907961
Sean Mullarkey <Seanmullarkey@u.boisestate.edu>
2020-05-19 14:42:02 -0600

    successful rolls implemented

--- Roll.py
+++ Roll.py
@@ -1,11 +1,41 @@
+'''
+Sean Mullarkey
+Yahtzee Dice Rolls
+'''
 import random
 
 class Roll:
 
-	def __init__(self):
-		self._current_dice_list = [] 
-        	self._current_kept_dice = [] 
 
+	def __init__(self):
+		self._current_dice_list = [6,6,6,6,6] 
+        	self._current_kept_dice = [] 
+		self._current_Roll = 3
+
+	def roll_dice(self): 
+		if self._current_Roll == 3:
+        		self._current_kept_dice *= 0 
+        		self._current_dice_list = [random.randint(1,6) for die in range(0,5)] 
+			self._current_Roll = self._current_Roll - 1 #increment num of times we rolled so far			
+			print 'Current Roll: ' + str(self._current_dice_list)[1:-1] + '\tRolls left: ' + str(self._current_Roll) 		 		
+        	else:
+        		self._current_dice_list = [random.randint(1,6) for die in range(0,5 - (len(self._current_kept_dice)))] 
+			self._current_Roll = self._current_Roll - 1 #increment num of times we rolled so far			
+			print 'Current Roll: ' + str(self._current_dice_list)[1:-1] + '\tRolls left: ' + str(self._current_Roll) 
+			if self._current_Roll == 0: # end of turn
+				print 'end of turn'
+				self._current_Roll = 3				 
+		
+		return self._current_dice_list 
+
+	def single_values(self,dice_list,check_value):
+    		roll_score = 0
+        	for die in dice_list:
+            		if die == check_value:
+                		roll_score +=die
+    		return roll_score 
+
+	#functions for special Rolls
 	def yahtzee(self, dice_list):    
 		if len(set(dice_list)) == 1: #no duplicates
         		return True
@@ -47,11 +77,5 @@ class Roll:
 			roll_score += die
 		return roll_score
 
-	def single_values(self,dice_list,check_value):
-    		roll_score = 0
-        	for die in dice_list:
-            		if die == check_value:
-                		roll_score +=die
-    		return roll_score 
 
 
