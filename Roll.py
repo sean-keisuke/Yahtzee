1228ba4acbe5251e4dbce93e17605a266012f8b2
Sean Mullarkey <Seanmullarkey@u.boisestate.edu>
2020-05-19 16:07:18 -0600

    implemented small straights and better large straight function

--- Roll.py
+++ Roll.py
@@ -18,24 +18,24 @@ class Roll:
 	def getKeep(self):
 		return self.currentKeep
 
-	def diceRoll(self): 
-		if self.currentRoll == 3:
+	def diceRoll(self): #Rolls for each turn
+		if self.currentRoll == 3: #first turn
         		self.currentKeep *= 0 
         		self.currentDiceList = [random.randint(1,6) for die in range(0,5)] 
 			self.currentRoll = self.currentRoll - 1 #Decrement num of times we rolled so far			
 			print 'Current Roll: ' + str(self.currentDiceList)[1:-1] + '\tRolls left: ' + str(self.currentRoll)
-        	else:
+        	else: # 2/3 turn
         		self.currentDiceList = [random.randint(1,6) for die in range(0,5 - (len(self.currentKeep)))] 
 			self.currentRoll = self.currentRoll - 1 #Decrement num of times we rolled so far			
 			print 'Current Roll: ' + str(self.currentDiceList)[1:-1] + '\tRolls left: ' + str(self.currentRoll) 
 			if self.currentRoll == 0: # end of turn
 				print 'end of turn'
-				self.lastTurn()
+				self.lastTurn() #keep the remaining dice
 				self.currentRoll = 3				 
 		
 		return self.currentDiceList 
 
-	def diceToKeep(self):
+	def diceToKeep(self): #user input decides which dices to keep after roll
 		keepInput = raw_input('which dice do you want to keep (comma separated: e.g. 1,1,5)? ')
 		keepInput = keepInput.split(',')
 
@@ -53,18 +53,19 @@ class Roll:
 
 		return self.currentDiceList
 
-	def lastTurn(self):
+	def lastTurn(self): #forced to keep everything before new round
 		self.currentKeep = self.currentDiceList + self.currentKeep
 		return self.currentKeep		
 
-	def upperScore(self,dice_list,check_value):
+	def upperScore(self,dice_list,check_value): #upper column (1's 2's etc)
     		roll_score = 0
         	for die in dice_list:
-            		if die == check_value:
-                		roll_score +=die
+            		if die == check_value: 
+                		roll_score += die
     		return roll_score 
 
-	#functions for special Rolls
+	# functions for special Rolls or lower region of scorecard
+
 	def yahtzee(self, dice_list):    
 		if len(set(dice_list)) == 1: #no duplicates
         		return True
@@ -76,11 +77,23 @@ class Roll:
 			return True
 		return False
 
-	def straight(self, dice_list): #2,3,4,5,6 or 1,2,3,4,5
+	def largeStraight(self, dice_list): #2,3,4,5,6 or 1,2,3,4,5
 		dice_list.sort()
-		if len(set(dice_list)) == 5 and dice_list[0] == 2 and dice_list[4] == 6:
+		if len(set(dice_list)) < 5:
+			return False
+		elif dice_list[4] - dice_list[0] == 4:
 			return True
-		elif len(set(dice_list)) == 5 and dice_list[0] == 1 and dice_list[4] == 5:
+		return False
+
+	def smallStraight(self, dice_list): #1,1,2,3,4 etc
+		dice_list.sort()
+		if len(set(dice_list)) < 4:
+			return False
+		elif dice_list[4] - dice_list[0] == 3: # 1,1,2,3,4 or 1,2,3,4,4 etc
+			return True
+		elif dice_list[3] - dice_list[0] == 3: # 1,2,3,4,6
+			return True		
+		elif dice_list[4] - dice_list[1] == 3: # 1,3,4,5,6
 			return True
 		return False
 	
